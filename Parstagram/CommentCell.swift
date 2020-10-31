beb2119ef9c606af573a35975955d109630025ad
Victor Miezah <vmiezah19@gmail.com>
2020-10-31 02:21:02 -0400

    Added commenting feature, user can also logout of their account and they stay logged in

---
+++ Parstagram/CommentCell.swift
@@ -0,0 +1,23 @@
+//
+//  CommentCell.swift
+//  Parstagram
+//
+//  Created by Victor Miezah on 10/30/20.
+//
+
+import UIKit
+
+class CommentCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
